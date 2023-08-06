0debd2c9b5b33b58b3e208a0921a7da7770eace2
Adam Ibhraheem <132217125+adamemadibrahim@users.noreply.github.com>
2023-08-06 19:57:14 +0800

    Create playChess.js
    for the js

---
+++ playChess.js
@@ -0,0 +1,25 @@
+var id = setInterval(frame, 5);
+
+function frame() {
+  if (/* test for finished */) {
+    clearInterval(id);
+  } else {
+    /* code to change the element style */ 
+  }
+}
+var id = null;
+function myMove() {
+  var elem = document.getElementById("abutton");
+  var pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.top = pos + 'px';
+      elem.style.left = pos + 'px';
+    }
+  }
+}
