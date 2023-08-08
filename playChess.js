c6b2edfc17ae837d016f7a62885f5d363b61447a
Shifraz Nazar <shifraznazar@gmail.com>
2023-08-08 23:06:15 +0800

    Update events.html

--- playChess.js
+++ playChess.js
@@ -1,23 +1,32 @@
 var id = null;
 
-  function myMove() {
-    var elem = document.getElementById("abutton");
-    var pos = 0;
-    clearInterval(id);
-    id = setInterval(frame, 10);
+function myMove() {
+  var elem = document.getElementById("abutton");
+  var pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 10);
 
-    function frame() {
-      if (pos >= 350) {
-        clearInterval(id);
-      } else {
-        pos++;
-        elem.style.top = pos + 'px';
-        elem.style.left = pos + 'px';
-      }
+  function frame() {
+    if (pos >= 350) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.top = pos + 'px';
+      elem.style.left = pos + 'px';
     }
   }
+}
 
-  // Call the myMove function when the page finishes loading.
-  window.onload = function () {
-    myMove();
-  };
+// Function to show a window alert
+function showAlert() {
+  alert("Get ready to play chess!");
+}
+
+// Call the myMove function when the page finishes loading.
+window.onload = function () {
+  myMove();
+
+  // Add click event listener to the "Play Now" button
+  var playButton = document.getElementById("abutton");
+  playButton.addEventListener("click", showAlert);
+};
